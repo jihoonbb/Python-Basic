0ef35d6cd053a1edad372fb6360aeeea3aa684b5
jihoonbb <1106jihoon@gmail.com>
2025-10-20 07:31:28 +0000

    20251020

--- variable.py
+++ variable.py
@@ -50,12 +50,35 @@
 
 
 # 자료형을 확인하는 방법
-# type() => 괄호안에 있는 변수 또는 값의 자료혀을 가지고옴
+# type() => 괄호안에 있는 변수 또는 값의 자료형을 가지고옴
 # type(변수)        <= 이렇게 사용하는 경우 프로그램으로 동작은 하지만 자료형을 눈으로 볼 수 없음
 # print(type(변수))
 # 위 형식으로 print()안에 넣어서 주로 사용
 
-int0 = 1                    # int0라는 변수에 int 자료형인 1 저장
-float0 = 3.14
-str0 = 'test'
-type(int0)                  # 동
+# int0 = 1                    # int0라는 변수에 int 자료형인 1 저장
+# float0 = 3.14
+# str0 = 'test'
+# type(int0)                  # 동작을 하지만 자료형을 눈으로 볼 수 없음
+# print(type(int0))           # type()을 사용하여 int0의 자료형 출력
+# print(type(float0))
+# print(type(str0))
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# 형 변환(casting)
+# str(변수 or 값)       => 변수 or 값을 str 자료형으로 변환
+# float(변수 or 값)     => 변수 or 값을 float 자료형으로 변환
+# int(변수 or 값)       => 변수 or 값을 int 자료형으로 변함
+# 단순히 연산할때만 사용하면 원본의 값은 변하지 않음
+# 원본의 자료형을 변화 시키기 위해서는 변수에 값을 다시 넣어야 함 [ ex, a = int(a) ]
+
+var1 = 2
+var2 = '31' 
+result = var1 + int(var2)   # result 변수에 var1 변수 = int 자료형으로 변환한 var2변수 저장
+print(result)
+print(type(var2))           # 변수 var2 자료형을 출력, result 변수에는 int 자료형으로 계산이 되었지만 
+                            # 실제 변수의 자료형은 변환 안됨
+var2 = int(var2)             # var2 변수에 int 자료형으로 변환한 var2 변수 저장
+print(type(var2))           # 변수 var2 자료형을 출력
